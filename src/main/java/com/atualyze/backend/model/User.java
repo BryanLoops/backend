1b54d8591e4947a6bcce84345b9d423c31c5a3b3
Bryan Lopes <bryan.lopes89@outlook.com>
2020-10-24 10:19:23 -0300

    Primeiro commit - Basics

---
+++ src/main/java/com/atualyze/backend/model/User.java
@@ -0,0 +1,92 @@
+package com.atualyze.backend.model;
+
+import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "usuario", uniqueConstraints = @UniqueConstraint(columnNames = "CPF"))
+public class User {
+    // Declarar atributos como variáveis
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String CPF;
+    private String nome;
+    private String nascimento;
+    private String email;
+    private String senha;
+    private String telefone;
+    private String endereco;
+
+    // Construtor
+    public User(String nome, String CPF, String nascimento, String email, String senha, String telefone, String endereco) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.nascimento = nascimento;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    // Getter e Setter
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
